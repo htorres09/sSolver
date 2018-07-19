1d31b68c67862f4e1f775f81ca27e0d7ce53c56a
htorres09 <lcc.h.torres09@gmail.com>
2018-07-19 08:15:42 -0500

    Data Changes
    Cambios en los datos

--- sSolver.js
+++ sSolver.js
@@ -1,21 +1,12 @@
-var cubo1 = ['#11', '#12', '#13', '#21', '#22', '#23', '#31', '#32', '#33' ]
-var cubo2 = ['#14', '#15', '#16', '#24', '#25', '#26', '#34', '#35', '#36' ]
-var cubo3 = ['#17', '#18', '#19', '#27', '#28', '#29', '#37', '#38', '#39' ]
-var cubo4 = ['#41', '#42', '#43', '#51', '#52', '#53', '#61', '#62', '#63' ]
-var cubo5 = ['#44', '#45', '#46', '#54', '#55', '#56', '#64', '#65', '#66' ]
-var cubo6 = ['#47', '#48', '#49', '#57', '#58', '#59', '#67', '#68', '#69' ]
-var cubo7 = ['#71', '#72', '#73', '#81', '#82', '#83', '#91', '#92', '#93' ]
-var cubo8 = ['#74', '#75', '#76', '#84', '#85', '#86', '#94', '#95', '#96' ]
-var cubo9 = ['#77', '#78', '#79', '#87', '#88', '#89', '#97', '#98', '#99' ]
-
+var cubo = []
+var fil = []
+var col = []
 var filSol = [];
 var colSol = [];
 var arregloFilas = [];
 var arregloColumnas = [];
 
 $(document).ready(function(){
-  console.log("Ready");
-  //$('.solucion').prop('disabled', true);
   inicializar();
   obtenerArreglos();
   for (s=1; s<10; s++){
@@ -38,26 +29,10 @@ $('#buscar').keyup(function() {
             if($(c).val()==valor && $(c).val()!=''){
                 //console.log(c+' '+valor);
                 iluminarCaja(i,j);
-                if(cubo1.includes(c)){
-                    iluminarCuadro('#C1',1);
-                } else if (cubo2.includes(c)) {
-                    iluminarCuadro('#C2',2);
-                } else if (cubo3.includes(c)) {
-                    iluminarCuadro('#C3',3);
-                } else if (cubo4.includes(c)) {
-                    iluminarCuadro('#C4',4);
-                } else if (cubo5.includes(c)) {
-                    iluminarCuadro('#C5',5);
-                } else if (cubo6.includes(c)) {
-                    iluminarCuadro('#C6',6);
-                } else if (cubo7.includes(c)) {
-                    iluminarCuadro('#C7',7);
-                } else if (cubo8.includes(c)) {
-                    iluminarCuadro('#C8',8);
-                } else if (cubo9.includes(c)) {
-                    iluminarCuadro('#C9',9);
-                } else {
-                    console.log("No existe");
+                for(n=0;n<9;n++){
+                  if(cubo[n].includes(c)){
+                    iluminarCuadro('#C'+(n+1),(n+1))
+                  }
                 }
             }
         }
@@ -70,6 +45,7 @@ $(':input').keyup(function() {
 });
 
 function inicializar(){
+  //Soluciones
   filSol[0] = [1,2,3,4,5,6,7,8,9]
   filSol[1] = [1,2,3,4,5,6,7,8,9]
   filSol[2] = [1,2,3,4,5,6,7,8,9]
@@ -89,6 +65,39 @@ function inicializar(){
   colSol[6] = [1,2,3,4,5,6,7,8,9]
   colSol[7] = [1,2,3,4,5,6,7,8,9]
   colSol[8] = [1,2,3,4,5,6,7,8,9]
+
+  //IDs Filas
+  fil[0] = ['#11', '#12', '#13', '#14', '#15', '#16', '#17', '#18', '#19' ]
+  fil[1] = ['#21', '#22', '#23', '#24', '#25', '#26', '#27', '#28', '#29' ]
+  fil[2] = ['#31', '#32', '#33', '#34', '#35', '#36', '#37', '#38', '#39' ]
+  fil[3] = ['#41', '#42', '#43', '#44', '#45', '#46', '#47', '#48', '#49' ]
+  fil[4] = ['#51', '#52', '#53', '#54', '#55', '#56', '#57', '#58', '#59' ]
+  fil[5] = ['#61', '#62', '#63', '#64', '#65', '#66', '#67', '#68', '#69' ]
+  fil[6] = ['#71', '#72', '#73', '#74', '#75', '#76', '#77', '#78', '#79' ]
+  fil[9] = ['#81', '#82', '#83', '#84', '#85', '#86', '#87', '#88', '#89' ]
+  fil[8] = ['#91', '#92', '#93', '#94', '#95', '#96', '#97', '#98', '#99' ]
+  //IDs columnas
+  col[0] = ['#11', '#21', '#31', '#41', '#51', '#61', '#71', '#81', '#91' ]
+  col[1] = ['#12', '#22', '#32', '#42', '#52', '#62', '#72', '#82', '#92' ]
+  col[2] = ['#13', '#23', '#33', '#43', '#53', '#63', '#73', '#83', '#93' ]
+  col[3] = ['#14', '#24', '#34', '#44', '#54', '#64', '#74', '#84', '#94' ]
+  col[4] = ['#15', '#25', '#35', '#45', '#55', '#65', '#75', '#85', '#95' ]
+  col[5] = ['#16', '#26', '#36', '#46', '#56', '#66', '#76', '#86', '#96' ]
+  col[6] = ['#17', '#27', '#37', '#47', '#57', '#67', '#77', '#87', '#97' ]
+  col[7] = ['#18', '#28', '#38', '#48', '#58', '#68', '#78', '#88', '#98' ]
+  col[8] = ['#19', '#29', '#39', '#49', '#59', '#69', '#79', '#89', '#99' ]
+
+  //IDs Cuadros
+  cubo[0] = ['#11', '#12', '#13', '#21', '#22', '#23', '#31', '#32', '#33' ]
+  cubo[1] = ['#14', '#15', '#16', '#24', '#25', '#26', '#34', '#35', '#36' ]
+  cubo[2] = ['#17', '#18', '#19', '#27', '#28', '#29', '#37', '#38', '#39' ]
+  cubo[3] = ['#41', '#42', '#43', '#51', '#52', '#53', '#61', '#62', '#63' ]
+  cubo[4] = ['#44', '#45', '#46', '#54', '#55', '#56', '#64', '#65', '#66' ]
+  cubo[5] = ['#47', '#48', '#49', '#57', '#58', '#59', '#67', '#68', '#69' ]
+  cubo[6] = ['#71', '#72', '#73', '#81', '#82', '#83', '#91', '#92', '#93' ]
+  cubo[7] = ['#74', '#75', '#76', '#84', '#85', '#86', '#94', '#95', '#96' ]
+  cubo[8] = ['#77', '#78', '#79', '#87', '#88', '#89', '#97', '#98', '#99' ]
+
 }
 
 function obtenerArreglos(){
@@ -183,20 +192,14 @@ function discriminarRestantes(){
   obtenerArreglos();
   //Remover elementos de la solución que estan en los arreglos fila y columna
   for(n=0; n<9; n++){
-    //console.log("Arreglo Filas: " + filSol[n].toString());
-    //console.log("Arreglo Columnas: " + colSol[n].toString());
     for(m=0; m<9; m++){
       if(filSol[n].includes(arregloFilas[n][m])){
-        //console.log("Elemento a eliminar: " + arregloFilas[n][m]);
         filSol[n].splice($.inArray(arregloFilas[n][m],filSol[n]),1);
       }
       if(colSol[n].includes(arregloColumnas[n][m])){
-        //console.log("Elemento a eliminar: " + arregloColumnas[n][m]);
         colSol[n].splice($.inArray(arregloColumnas[n][m],colSol[n]),1);
       }
     }
-    //console.log("Arreglo Filas: " + filSol[n].toString());
-    //console.log("Arreglo Columnas: " + filSol[n].toString());
     v = n+1;
     $("#sf"+v).val(filSol[n]);
     $("#sc"+v).val(colSol[n]);
@@ -204,78 +207,76 @@ function discriminarRestantes(){
 }
 
 function verificar() {
-    verificarCuadro();
-    //verificarLineas();
+    //verificarCuadro();
+    verificarLineas();
 }
 
 function verificarCuadro() {
-    var cuadro1 = [];
-    var cuadro2 = [];
-    var cuadro3 = [];
-    var cuadro4 = [];
-    var cuadro5 = [];
-    var cuadro6 = [];
-    var cuadro7 = [];
-    var cuadro8 = [];
-    var cuadro9 = [];
+    var cuadro = []
     var ids = [];
-    var valoresActuales = [];
+    for(k=0; k<9; k++){
+      cuadro[k] = [];
+    }
 
     // Obtener arreglos de los 9 cuadros
-    for(e=0; e<9; e++){
-        cuadro1.push($(cubo1[e]).val());
-        cuadro2.push($(cubo2[e]).val());
-        cuadro3.push($(cubo3[e]).val());
-        cuadro4.push($(cubo4[e]).val());
-        cuadro5.push($(cubo5[e]).val());
-        cuadro6.push($(cubo6[e]).val());
-        cuadro7.push($(cubo7[e]).val());
-        cuadro8.push($(cubo8[e]).val());
-        cuadro9.push($(cubo9[e]).val());
+    for(i=0; i<9; i++){
+      for(j=0; j<9; j++){
+        cuadro[i].push($(cubo[i][j]).val());
+      }
     }
 
     // Obtener los ids de las cajas repetidas
-    ids.push(comparar(cuadro1));
+    for(l=0; l<9; l++){
+      ids = ids.concat(comparar(cuadro[l], cubo[l]));
+    }
+
     // Agrega clase a los ids
-    for(n in ids){
-        if(!($(ids[n]).hasClass('highbox'))){
-            $(ids[n]).addClass('higherror');
-        }
-    }
-}
-
-function comparar(arr){
-    var valoresActuales = [];
-    var indices = [];
-
-    for(i=0; i<arr.length; i++){
-        var valor = arr[i];
-        if(valor in valoresActuales){
-            indices.push(i);
-        } else {
-            valoresActuales.push(valor);
-        }
-    }
-    return indices;
+    agregarClase(ids, 'higherror');
 }
 
 function verificarLineas() {
     var ids = [];
-    var id = '';
     // Obtener arreglos de los 9 cuadros
     obtenerArreglos();
-    for(n=0; n<9; n++){
-        for(m=0; m<9; m++){
-            if(arregloFilas[n][m] == n+1){
 
-            }
-        }
+    // Obtener las ids de cajas repetidas en linea
+    for(n=0; n<9; n++){
+      ids = ids.concat(comparar(arregloFilas[n], fil[n]));
+    }
+    // Obtener las ids de cajas repetidas en columna
+    for(n=0; n<9; n++){
+      ids = ids.concat(comparar(arregloColumnas[n], col[n]));
     }
 
     // Agrega clase a los ids
-    for(n in ids){
-        if(!($(ids[n]).hasClass('highbox'))){
-            $(ids[n]).addClass('higherror');
+    agregarClase(ids, 'higherror');
+}
+
+function comparar(arrCubo, arrIds){
+  var indices = [];
+  for(i=0; i<arrCubo.length; i++){
+    if(arrCubo[i]!=''){
+      for(j=0; j<arrCubo.length; j++){
+        if(arrCubo[j]!='' && arrCubo[i]!=''){
+          if(arrCubo[j]==arrCubo[i]){
+            if(!(indices.includes(arrIds[i]))){
+              indices.push(arrIds[i]);
+            }
+            if(!(indices.includes(arrIds[j]))){
+              indices.push(arrIds[j]);
+            }
+          }
         }
+      }
     }
+  }
+  return indices;
+}
+
+function agregarClase(ids, clase){
+  for(n in ids){
+      if(!($(ids[n]).hasClass('highbox'))){
+          $(ids[n]).addClass(clase);
+      }
+  }
 }
